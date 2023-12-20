b0622537557d967dc33b78d9c6f525abc1111860
rjalaparthi <jalaparthiphaniraghava@gmail.com>
2023-12-20 09:32:22 -0500

    Changed Variable Name

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PERSON1=Raghava
-PERSON2=Riyan
+PERSON2=Ram
 echo "$PERSON1:Hello $PERSON2, Good Morning"
 echo "$PERSON2 :Hello $PERSON1 , very good morning"
 echo "$PERSON1 : How are you $PERSON2"
