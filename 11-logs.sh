820227b7650af2fca1990b3ce1e5ddabc67f17b4
rjalaparthi <jalaparthiphaniraghava@gmail.com>
2023-12-20 17:36:29 -0500

    Adding Logs

--- 11-logs.sh
+++ 11-logs.sh
@@ -26,7 +26,7 @@ fi
 
 if [ $ID -ne 0 ]
 then
-    echo "Error :: Please run this script with root access"
+    echo -e "Error :: $R Please run this script with root access"
     exit 1 # you can give other than 0 and it won't proceed
 else
     echo "You are root user"
