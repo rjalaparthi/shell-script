4ff0beed689ad19c1d225db01f1298cee6f532d2
rjalaparthi <jalaparthiphaniraghava@gmail.com>
2023-12-20 16:50:50 -0500

    Adding Logs

---
+++ 11-logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+ID=$(id -u)
+
+LOG_FILE=/tmp/
+echo "script name is $0"
+VALIDATE(){
+if  [ $1 -ne 0 ]
+then
+    echo "Error :: $2 ... Failed"
+    exit 1
+else 
+    echo "installing $2 is sucessfull"
+fi 
+
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "Error :: Please run this script with root access"
+    exit 1 # you can give other than 0 and it won't proceed
+else
+    echo "You are root user"
+fi #reverse of if, indication condition is end
+
+yum install mysql -y
+
+VALIDATE $? "Installing mysql"
+
+yum install git -y
+
+VALIDATE $? "Installing git"
