6d83a1f1fca3c0f8e71f3bad7fb2d5ac1527a108
rjalaparthi <jalaparthiphaniraghava@gmail.com>
2023-12-20 09:28:42 -0500

    Added Varibles and printed convo using variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-echo "Raghava:Hello Riyan, Good Morning"
-echo "Riyan :Hello Raghava , very good morning"
-echo "Raghava : How are you Riyan"
-echo "Riyan :I am good raghava, how are you"
+PERSON1=Raghava
+PERSON2=Riyan
+echo "$PERSON1:Hello $PERSON2, Good Morning"
+echo "$PERSON2 :Hello $PERSON1 , very good morning"
+echo "$PERSON1 : How are you $PERSON2"
+echo "$PERSON2 :I am good $PERSON1, how are you"
