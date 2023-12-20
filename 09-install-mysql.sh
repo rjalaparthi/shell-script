a7617f84ecc42dc538c89234c49530b4c88006de
rjalaparthi <jalaparthiphaniraghava@gmail.com>
2023-12-20 16:07:43 -0500

    Installing msql

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -2,7 +2,18 @@
 ID=$(id -u)
 if [ $ID -ne 0 ]
 then
-echo "Error :: Please run this script with root access"
+    echo "Error :: Please run this script with root access"
+    exit 1 # you can give other than 0 and it won't proceed
 else
-echo "You are root user"
-fi
+    echo "You are root user"
+fi #reverse of if
+
+yum install mysql -y
+
+if[ $? -ne 0 ]
+then
+    echo "Error :: Installing mysql is failed"
+    exit 1
+else 
+    echo "installing mysql is sucessfull"
+fi    
