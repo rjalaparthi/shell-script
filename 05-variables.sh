7a11448d7c2c852a3dad4ec4ab55a9b07743bbca
rjalaparthi <jalaparthiphaniraghava@gmail.com>
2023-12-20 10:07:17 -0500

    Added read command

---
+++ 05-variables.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+echo "Please enter username"
+read USERNAME #value enetered above will be stored in USERNAME
+echo "Please eneter password"
+read PASS #Value above entered stored in PASS
+echo "User Name is : $USERNAME"
+echo "Password is : $PASS"
