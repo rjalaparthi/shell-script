5785c8902e9d80d1f545c6f0411927c5e84eb8c5
rjalaparthi <jalaparthiphaniraghava@gmail.com>
2023-12-20 10:11:38 -0500

    Added read -s command

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,7 +2,9 @@
 
 echo "Please enter username"
 read -s USERNAME #value enetered above will be stored in USERNAME
+
 echo "Please eneter password"
 read -s PASS #Value above entered stored in PASS
+
 echo "User Name is : $USERNAME" # i am printing for testing purpose, usually we won't print
 echo "Password is : $PASS"
