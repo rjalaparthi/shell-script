8be4fef2f16bde24828737911bf98ccaa7e4abce
rjalaparthi <jalaparthiphaniraghava@gmail.com>
2023-12-20 09:58:36 -0500

    Added Argument

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
+echo "$PERSON1:Hello $PERSON2, Good Morning"
+echo "$PERSON2 :Hello $PERSON1 , very good morning"
+echo "$PERSON1 : How are you $PERSON2"
+echo "$PERSON2 :I am good $PERSON1, how are you"
+echo "$PERSON3 :Hello i am here please talk with me"
